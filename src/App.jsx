58215cd9c23af89f6725ee5fbe5212e67862ecaa
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-10-28 20:50:12 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React, { useState } from 'react';
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Alllist from './components/Alllist';
-import Fsd from './components/Fsd';
-import Datascience from './components/Datascience';
-import Cyber from './components/Cyber';
+import CardList from './components/CardList';
 import Career from './components/Career';
-import Card1 from './components/Card1';
-import Card2 from './components/Card2';
-import Card3 from './components/Card3';
-import Card1Ds from './components/Card1Ds';
-import Card2Ds from './components/Card2Ds';
-import Card1Cy from './components/Card1Cy';
-import Card2cy from './components/Card2cy';
-import CardCar from './components/CardCar';
+import Cyber from './components/Cyber';
+import Fsd from './components/Fsd';
+import Datascience from './components/Datascience'
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -31,11 +23,11 @@ function App() {
               Menu
             </button>
             {menuOpen && (
-              <div className="mobile-menu d-inline-block" >
+              <div className="mobile-menu d-inline-block">
                 <Link to='/' style={{ padding: 10 }} onClick={toggleMenu}>
                   ALL
                 </Link>
-                <Link to='/fsd'className='d-inline-block' style={{ padding: 10 }} onClick={toggleMenu}>
+                <Link to='/fsd' className='d-inline-block' style={{ padding: 10 }} onClick={toggleMenu}>
                   FULL STACK DEVELOPMENT
                 </Link>
                 <Link to='/datascience'className='d-inline-block' style={{ padding: 10 }} onClick={toggleMenu}>
@@ -57,24 +49,16 @@ function App() {
             <Link to='/cyber' style={{ padding: 10 }}>CYBER SECURITY</Link>
             <Link to='/career' style={{ padding: 10 }}>CAREER</Link>
             <hr />
+            <hr />
           </div>
         </div>
         <Routes>
-          <Route path='/' element={<Alllist />} />
+          <Route path='/' element={<CardList />} />
           <Route path='/fsd' element={<Fsd />} />
           <Route path='/datascience' element={<Datascience />} />
           <Route path='/cyber' element={<Cyber />} />
           <Route path='/career' element={<Career />} />
-          <Route path='/fsd/card1' element={<Card1 />} />
-          <Route path='/fsd/card2' element={<Card2 />} />
-          <Route path='/fsd/card3' element={<Card3 />} />
-          <Route path='/ds/card1Ds' element={<Card1Ds />} />
-          <Route path='/ds/card2Ds' element={<Card2Ds />} />
-          <Route path='/cyber/card1Cy' element={<Card1Cy />} />
-          <Route path='/cyber/card2Cy' element={<Card2cy />} />
-          <Route path='/career/CardCar' element={<CardCar />} />
         </Routes>
-
       </Router>
     </div>
   );
---
+++ src/components/CardList.jsx
@@ -0,0 +1,254 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function CardList() {
+  return (
+    <div className="container mt-4 card-group">
+      <h1 className="text-center">Browse Our Card Collection</h1>
+      <div className="row card-deck">
+        {/* Card 1 */}
+        <div className="col-md-4 card-group d-inline-block">
+          <div className="card ">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/07/best-way-to-learn-full-stack-development.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">Best Ways to Learn Full Stack Development in 2023</h5>
+              <p className="card-text">
+                Full-stack development has become increasingly popular in recent years, with companies seeking professionals who can handle both front-end and back-end tasks.Today’s world is rapidly evolving into a technological landscape. Owing to these dynamics, the demand and scope for full-stack developers are constantly on the rise. Are you an aspiring web developer striving to enhance your web development skills and become a full-stack web developer?
+               
+                If yes, you’ve come to the right place. In this comprehensive blog, you can find the list of the best full-stack development online courses available on the internet in 2023. Whether you’re a beginner or already have some programming experience, the online courses mentioned in this blog can provide you with the knowledge and skills needed to thrive as a full-stack developer.
+
+                So, look no further, choose the right course from the list below, and get started!
+              </p>
+              <Link to="/fsd/card2" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 2 */}
+        <div className="col-md-4 card-group d-inline-block">
+          <div className="card">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2022/12/Yellow-Blue-Illustration-Business-Blog-Banner.png"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">Cybersecurity Vs Ethical Hacking: Top 10 Differences</h5>
+              <p className="card-text">
+                Cybersecurity & Ethical hacking have been key in making sure that your data online is secure, users are authenticated, and your data and privacy concerns are all kept safe.
+                1. Python is an incredibly versatile programming language. It’s not limited to just one domain but can be used for a wide range of applications, including web development, automation, scientific computing, and, of course, data science. This versatility means that if you learn Python for data science, you’re also gaining a valuable skill that can be applied in various other fields.
+
+                2. Python has a vast and active community of users and developers. This means that there are countless resources available, including libraries, frameworks, and tutorials, to support data scientists. The open-source nature of Python encourages collaboration, which leads to the rapid development of tools and solutions tailored to data science needs.
+
+                3. Python is known for its clean and readable syntax. Its code is easy to understand, even for beginners, which makes it an ideal choice for those new to programming and data science. This user-friendly syntax reduces the learning curve, allowing data science enthusiasts to focus on the data and algorithms rather than struggling with complex code.
+
+
+              </p>
+              <Link to="/cyber/card1Cy" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 3 */}
+        <div className="col-md-4 card-group d-inline-block">
+          <div className="card">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-Useful-Python-Libraries-and-Tools-For-Data-Science-Beginners.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">Useful Python Libraries & Tools for Data Science Beginners</h5>
+              <p className="card-text">
+                In a world filled with information, knowing how to understand and use data is super important. Data science, which is all about finding valuable insights from data, is a skill that’s needed in many areas like business, finance, and healthcare.
+
+                1. Python offers a rich ecosystem of libraries and frameworks that cater specifically to data science. Libraries like NumPy, Pandas, Matplotlib, and Scikit-Learn provide powerful tools for data manipulation, analysis, visualization, and machine learning. These libraries simplify complex tasks and accelerate the development of data science projects.
+
+                2. Python seamlessly integrates with other languages and tools commonly used in data science, such as R, SQL, and Jupyter Notebooks. This integration enables data scientists to leverage the best tools for various tasks within a single environment.
+
+                3. Python libraries like Matplotlib, Seaborn, and Plotly offer robust capabilities for data visualization. Visualizations are essential for understanding data patterns and communicating findings effectively.
+
+                4. Python is the language of choice for machine learning and deep learning projects. Libraries like Scikit-Learn and TensorFlow provide extensive support for building and training models.
+              </p>
+              <Link to="/ds/card2Ds" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 4 */}
+        <div className="col-md-4 card-group d-inline-block">
+          <div className="card">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">The Top 10 Tools Every Full-Stack Developer Should Master in 2023</h5>
+              <p className="card-text">
+                As a full-stack developer, having the right set of tools is crucial for your success. In the ever-evolving world of web development, staying updated with the latest tools can significantly enhance your productivity and make you a more competent developer.
+                Choosing the right full-stack development online course can be a crucial decision that impacts your learning journey and future career. Selecting the best full-stack development online course depends on your current skill level, learning preferences, and career goals.
+
+                While selecting a Full-stack Development course, consider factors such as the technologies covered, the teaching style of the instructor, availability of practical projects, certifications, reviews, and recommendations from past students. To learn full-stack development, you need abundant dedication, practice, and continuous learning.
+              </p>
+              <Link to="/fsd/card3" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 5 */}
+        <div className="col-md-4 card-group d-inline-block">
+          <div className="card">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/10/Feature-image-Top-Product-Based-Companies-for-Digital-Marketers.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">Top Product-Based Companies for Digital Marketers</h5>
+              <p className="card-text">
+                Selecting the right launchpad for a successful career in digital marketing isn’t just about joining any company; it’s about aligning with those who are at the forefront of innovation.
+                At the core of a product-based company lies a tangible or intangible offering—something that captures essence and function in a tangible form. 
+
+                Think of Apple with its trailblazing devices or Netflix with its captivating shows. Unlike service-oriented firms, these businesses revolve around their primary products or a collection of related offerings. 
+
+                For digital marketers, this means diving deep into the very fabric of a product, unraveling its narratives, features, and the value it brings to consumers. 
+
+                When top digital marketers immerse themselves in a product-centric realm, like Adobe, their campaigns go beyond mere advertising. They’re painting a canvas of creativity, narrating tales of innovation, and highlighting a world of possibilities awaiting the user.
+              </p>
+              <Link to="/career/CardCar" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 6 */}
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">7 Best Full-Stack Development Online Courses [2023]</h5>
+              <p className="card-text">
+                Today’s world is rapidly evolving into a technological landscape. Owing to these dynamics, the demand
+                IBM Full Stack Software Developer Professional Certificate available on Coursera is a complete course instructed by 9+ IBM instructors. In this course, you can learn from basic concepts like HTML, CSS, and JavaScript to advanced-level project deployments. Developed by IBM experts, the course content reflects industry best practices and the latest technologies, ensuring students learn relevant and up-to-date skills.
+
+                This is a 4-month beginner-level course that can be learned on a flexible schedule. Upon successful completion of the program, participants receive a verified professional certificate from IBM, which can enhance their resumes and demonstrate their skills to potential employers.
+              </p>
+              <Link to="/fsd/card1" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+              {/* Card 7 */}
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="	https://www.guvi.in/blog/wp-content/uploads/2023/07/html-css-collage-concept-with-person-768x512.jpg"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">The Ultimate Guide to Real-World Full-Stack Development Applications</h5>
+              <p className="card-text">
+                Full-stack development has become increasingly popular in recent years, with companies seeking professionals who can
+                The Full Stack Web Developer Nanodegree by Udacity is one of the best full-stack development online courses that covers a wide range of topics essential for you to full-stack developer. The curriculum includes both front-end and back-end technologies. Throughout the course, you’ll work on hands-on projects that simulate real-world scenarios. These projects are designed to reinforce the concepts you’ve learned and help you build a portfolio of practical work that showcases your skills to potential employers.
+
+                The program is designed in collaboration with industry experts, ensuring that the skills you acquire are up-to-date and relevant to the current demands of the tech industry. You’ll learn front-end and back-end technologies like HTML, CSS, JavaScript, responsive design principles, Python, Flask, SQL, and authentication systems. This is a 4-months long course that offers a Nanodegree certification, upon successful completion.
+              </p>
+              <Link to="/fsd/card2" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+              {/* Card 8 */}
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2022/12/cybersecuity-.gif"
+              className="card-img-top"
+              alt="Importance and its uses & the growing challenges in 2023!"
+            />
+            <div className="card-body">
+              <h5 className="card-title">What is Cybersecurity? Importance and its uses & the growing challenges in 2023!</h5>
+            <p className="card-text">
+                Look around today, you will witness that we are more reliant on technology and devices than ever before. Benefits of this trend range from getting rid of Yellowbook, and having near-instant access to information on the internet o the modern conveniences provided by smart home automation technology and concepts like IoT devices. 
+                With so many great things coming from technology, it’s highly likely that some potential threats lurk behind every device & platform. Despite the world’s optimistic perception of modern devices, cyber threats possess an inevitable danger. 
+              </p>
+              <Link to="/custom/card8" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+              {/* Card 9 */}
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2021/03/Blog-9-768x402.png" 
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">The Cybersecurity Surge: 5 Must-Have Cybersecurity Certifications!</h5>
+              <p className="card-text">
+                There is something fascinating about a lone hacker in black hoodies using a single system and their skills to take down big corporations and their crony capitalism. The anecdote of a hacker has cultivated an ethos of individuality, corporate freedom, and distinct craftiness that can’t be denied. Also, the same archetype is visible in a number of pop-cultural references whether the famous TV series- Mr. Robot or real-life adaptations of Snowden and Julian Assange. Despite all the fascination of hacking, and hysteria, the field of cybersecurity is largely misunderstood outside of the realm of the IT ecosystem.
+              </p>
+              <Link to="/custom/card9" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+              {/* Card 10 */}
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-Everything-You-Should-Know-About-ChatGPT-and-Why-It-Matters-768x480.webp" 
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">Everything You Should Know About ChatGPT & Why It Matters?</h5>
+              <p className="card-text">
+                In the constantly growing landscape of artificial intelligence (AI) and natural language processing (NLP), ChatGPT has emerged as a significant milestone. ChatGPT is an advanced language model, developed by OpenAI, represents a leap forward for humankind, in our ability to interact with machines using natural language.
+
+                This revolutionary language model developed by Open AI, an American AI research and deployment company has captured the minds of tech enthusiasts, students, teachers, businesses, researchers, and everyone. But, does it impact your career? What are its applications? does it enhance your quality of work? Why does it even matter?
+
+                Find your answers in this blog that will delve into the world of ChatGPT, exploring its origins, capabilities, applications, and ethical implications. Keep Reading…
+              </p>
+              <Link to="/custom/card10" className="btn btn-primary">
+                Read More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Add more cards as needed */
+        }
+      </div>
+    </div>
+  );
+}
